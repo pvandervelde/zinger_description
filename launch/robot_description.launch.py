1276e15240d10dd52ebb4d2a9bdcb0fedbf7b4ed
Petrik van der Velde <petrikvandervelde@gmail.com>
2022-08-06 00:06:46 +1200

    Adding the launch file

---
+++ launch/robot_description.launch.py
@@ -0,0 +1,37 @@
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, PathJoinSubstitution
+from launch.substitutions.launch_configuration import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+ARGUMENTS = [
+    DeclareLaunchArgument('use_sim_time', default_value='false',
+                          choices=['true', 'false'],
+                          description='use_sim_time')
+]
+
+
+def generate_launch_description():
+    pkg_robot_description = get_package_share_directory('cratebot_description')
+    xacro_file = PathJoinSubstitution([pkg_robot_description,
+                                       'urdf',
+                                       'base.xacro'])
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+            {'robot_description': Command(['xacro', ' ', xacro_file, ' ', 'gazebo:=ignition'])},
+        ],
+    )
+
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(robot_state_publisher)
+    return ld
